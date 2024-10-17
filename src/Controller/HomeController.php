833e8742a1814c7d2afdb2dd7e864cd8f18c9171
OLIVREAU <yolivreau@beaupeyrat.fr>
2024-10-17 11:56:30 +0200

    Message de commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,11 +11,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(PostRepository $PostRepository ): Response
+    public function index(PostRepository $postRepository): Response
     {
-        $posts = $PostRepository->findAll();
+        $posts = $postRepository->findAll();
+
+        // Récupération des catégories existantes dans les posts
+        $categories = array_unique(array_map(function ($post) {
+            return $post->getCategory();
+        }, $posts));
+
+        // Filtrage par catégorie si la catégorie est définie dans la requête
+        $selectedCategory = $_GET['category'] ?? null;
+        if ($selectedCategory) {
+            $posts = array_filter($posts, function ($post) use ($selectedCategory) {
+                return $post->getCategory() === $selectedCategory;
+            });
+        }
+
         return $this->render('home/index.html.twig', [
             'posts' => $posts,
+            'categories' => $categories,
         ]);
     }
 }
