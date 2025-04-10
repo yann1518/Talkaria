96905c3e8e196e9bd50f2b3cf2578b62973e7c31
OLIVREAU <yolivreau@beaupeyrat.fr>
2025-04-10 08:38:21 +0200

    token from username to email

---
+++ src/EventSubscriber/JWTCreatedSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+    namespace App\EventSubscriber;
+
+    use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+    use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+    class JWTCreatedSubscriber implements EventSubscriberInterface
+    {
+        public static function getSubscribedEvents(): array
+        {
+            return [
+                JWTCreatedEvent::class => 'onJWTCreated',
+            ];
+        }
+
+        public function onJWTCreated(JWTCreatedEvent $event): void
+        {
+            $user = $event->getUser();
+
+            $payload = $event->getData();
+            $payload['email'] = $user->getUserIdentifier();
+            unset($payload['username']);
+
+            $event->setData($payload);
+        }
+    }
