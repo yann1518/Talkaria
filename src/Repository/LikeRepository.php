00f093ee19d11f62008bfce5a27fa9ec6859b3fb
Yann <olivreauy@gmail.com>
2025-06-03 22:57:26 +0200

    likes

---
+++ src/Repository/LikeRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Like;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Like>
+ */
+class LikeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Like::class);
+    }
+
+    // Ajoute ici tes méthodes custom si besoin
+}
